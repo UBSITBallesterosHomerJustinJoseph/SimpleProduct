6dcc14407e1ca2fd0d1a49c9f9d175b1e9e8ac40
Homer Justin Joseph Y. Ballesteros <20200669@s.ubaguio.edu>
2025-09-04 21:12:35 +0800

    sync local

--- src/app/home/home.ts
+++ src/app/home/home.ts
@@ -1,10 +1,10 @@
 import { Component } from '@angular/core';
-import { RouterOutlet, RouterModule } from '@angular/router';
+import {  RouterModule } from '@angular/router';
 
 @Component({
   selector: 'app-home',
   standalone:true,
-  imports: [RouterOutlet, RouterModule],
+  imports: [ RouterModule],
   templateUrl: './home.html',
   styleUrl: './home.css'
 })
